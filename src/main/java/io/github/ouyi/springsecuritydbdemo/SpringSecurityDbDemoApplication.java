1c72658546bb3da58a16f0e530ef502347955e40
Yi Ou <ouyi@gmx.de>
2018-03-19 20:21:16 +0000

    Init commit

---
+++ src/main/java/io/github/ouyi/springsecuritydbdemo/SpringSecurityDbDemoApplication.java
@@ -0,0 +1,12 @@
+package io.github.ouyi.springsecuritydbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDbDemoApplication.class, args);
+	}
+}
