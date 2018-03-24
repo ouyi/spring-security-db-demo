32775ddfc37e65a644099daf3109963044c3c297
Yi Ou <ouyi@gmx.de>
2018-03-24 21:48:11 +0000

    Remove debugging output

--- src/main/java/io/github/ouyi/springsecuritydbdemo/service/UserDetailsImpl.java
+++ src/main/java/io/github/ouyi/springsecuritydbdemo/service/UserDetailsImpl.java
@@ -1,19 +1,16 @@
 package io.github.ouyi.springsecuritydbdemo.service;
 
-import io.github.ouyi.springsecuritydbdemo.model.Role;
 import io.github.ouyi.springsecuritydbdemo.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class UserDetailsImpl implements UserDetails {
 
     private final User user;
-
 
     UserDetailsImpl(User user) {
         this.user = user;
@@ -21,15 +18,6 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-
-        System.out.println("###############");
-        System.out.println(this.user.getRoles());
-
-        for (Role role : this.user.getRoles()
-             ) {
-            System.out.println(role.getRole());
-        }
-
         return this.user
                 .getRoles()
                 .stream()
